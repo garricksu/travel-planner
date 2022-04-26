8daeccc7838563646a52e00b896158ced871c434
Garrick <garrickdsu@gmail.com>
2022-04-25 20:53:08 -0700

    refactored user form input into separate component

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -2,22 +2,7 @@ import { Formik, useField } from 'formik'
 import { Button, Col, Container, Form, FormGroup, Row } from 'react-bootstrap'
 import * as Yup from 'yup'
 import { useRegisterUserMutation } from '../generated/graphql'
-
-// const UserFormInput = ({ label, ...props }) => {
-//   // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-//   // which we can spread on <input>. We can use field meta to show an error
-//   // message if the field is invalid and it has been touched (i.e. visited)
-//   const [field, meta] = useField(props)
-//   return (
-//     <>
-//       <label htmlFor={props.id || props.name}>{label}</label>
-//       <input className='text-input' {...field} {...props} />
-//       {meta.touched && meta.error ? (
-//         <div className='error'>{meta.error}</div>
-//       ) : null}
-//     </>
-//   )
-// }
+import { UserFormInput } from '../utils/UserFormInput'
 
 export const Register = () => {
   const [register] = useRegisterUserMutation()
@@ -50,58 +35,19 @@ export const Register = () => {
       >
         {(formik) => (
           <Form className='user-form' onSubmit={formik.handleSubmit}>
+            <Row></Row>
             <Row className='mb-3'>
-              <Col>
-                <Form.Label htmlFor='firstName'>First Name</Form.Label>
-                <Form.Control
-                  name='firstName'
-                  type='text'
-                  onChange={formik.handleChange}
-                  value={formik.values.firstName}
-                />
-              </Col>
-              <Col>
-                <Form.Label htmlFor='lastName'>Last Name</Form.Label>
-                <Form.Control
-                  name='lastName'
-                  type='text'
-                  onChange={formik.handleChange}
-                  value={formik.values.lastName}
-                />
-              </Col>
+              <UserFormInput label='First name' name='firstName' type='text' />
+              <UserFormInput label='Last name' name='lastName' type='text' />
             </Row>
             <Row className='mb-3'>
-              <Col>
-                <Form.Label htmlFor='username'>Username</Form.Label>
-                <Form.Control
-                  name='username'
-                  type='text'
-                  onChange={formik.handleChange}
-                  value={formik.values.username}
-                />
-              </Col>
+              <UserFormInput label='Username' name='username' type='text' />
             </Row>
             <Row className='mb-3'>
-              <Col>
-                <Form.Label htmlFor='email'>Email Address</Form.Label>
-                <Form.Control
-                  name='email'
-                  type='email'
-                  onChange={formik.handleChange}
-                  value={formik.values.email}
-                />
-              </Col>
+              <UserFormInput label='Email' name='email' type='text' />
             </Row>
             <Row className='mb-3'>
-              <Col>
-                <Form.Label htmlFor='password'>Password</Form.Label>
-                <Form.Control
-                  name='password'
-                  type='password'
-                  onChange={formik.handleChange}
-                  value={formik.values.password}
-                />
-              </Col>
+              <UserFormInput label='Password' name='password' type='password' />
             </Row>
             <Row className='mb-3'>
               <Col>
--- client/src/utils/UserFormInput.tsx
+++ client/src/utils/UserFormInput.tsx
@@ -1,9 +1,34 @@
+import { useField } from 'formik'
 import React from 'react'
+import { Col, FormControl, FormLabel, Row } from 'react-bootstrap'
 
 interface UserFormInputProps {
-
+  label: string
+  name: string
+  type: string
 }
 
-export const UserFormInput: React.FC<UserFormInputProps> = ({}) => {
-    return (<div></div>)
+export const UserFormInput: React.FC<UserFormInputProps> = ({
+  label,
+  ...props
+}) => {
+  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+  // which we can spread on <input>. We can use field meta to show an error
+  // message if the field is invalid and it has been touched (i.e. visited)
+  const [field, meta] = useField(props)
+  return (
+    <Col>
+      <Row>
+        <Col>
+          <FormLabel htmlFor={props.name}>{label}</FormLabel>
+        </Col>
+        <Col>
+          {meta.touched && meta.error ? (
+            <div className='text-danger text-end'>{meta.error}</div>
+          ) : null}
+        </Col>
+      </Row>
+      <FormControl {...field} {...props} name='firstName' type='text' />
+    </Col>
+  )
 }
